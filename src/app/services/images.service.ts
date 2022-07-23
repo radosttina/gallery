adddfabc69a4c68811db64e16868af2872065a1b
radosttina <radosttina@users.noreply.github.com>
2022-07-23 12:57:36 +0300

    Add error handling for GET images

--- src/app/services/images.service.ts
+++ src/app/services/images.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Image } from '../models/image.model';
 import { environment } from 'src/environments/environment';
+import { catchError, of, retry } from 'rxjs';
 
 const API_PATH = environment.apiPath;
 
@@ -9,7 +10,10 @@ const API_PATH = environment.apiPath;
   providedIn: 'root',
 })
 export class ImagesService {
-  images$ = this.http.get<Image[]>(`${API_PATH}/images`);
+  images$ = this.http.get<Image[]>(`${API_PATH}/images`).pipe(
+    catchError(() => of([])),
+    retry(3)
+  );
 
   constructor(private http: HttpClient) {}
 }
