fc5f052f9f50afad7e4b072d00bdf672d818036c
radosttina <radosttina@users.noreply.github.com>
2022-07-23 12:57:40 +0300

    Add routing to upload screen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,9 +9,11 @@ import { ImageComponent } from './image/image.component';
 import { ImagesService } from './services/images.service';
 import { UploadButtonComponent } from './upload-button/upload-button.component';
 import { UploadFormComponent } from './upload-form/upload-form.component';
+import { HomeComponent } from './home/home.component';
+import { UploadScreenComponent } from './upload-screen/upload-screen.component';
 
 @NgModule({
-  declarations: [AppComponent, ImageListComponent, ImageComponent, UploadButtonComponent, UploadFormComponent],
+  declarations: [AppComponent, ImageListComponent, ImageComponent, UploadButtonComponent, UploadFormComponent, HomeComponent, UploadScreenComponent],
   imports: [BrowserModule, HttpClientModule, AppRoutingModule],
   providers: [ImagesService],
   bootstrap: [AppComponent],
