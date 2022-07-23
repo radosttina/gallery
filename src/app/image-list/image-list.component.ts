ae9729640f7b64fa8b2da96fac06243416de48bd
radosttina <radosttina@users.noreply.github.com>
2022-07-23 12:57:36 +0300

    Change ChangaDetectionStrategy for image-list component

--- src/app/image-list/image-list.component.ts
+++ src/app/image-list/image-list.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 import { ImagesService } from '../services/images.service';
 
 @Component({
   selector: 'app-image-list',
   templateUrl: './image-list.component.html',
   styleUrls: ['./image-list.component.less'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ImageListComponent implements OnInit {
   images$ = this.service.images$;
